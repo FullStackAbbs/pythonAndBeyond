ddfde12d87b3e4d380963d5af4d957b2f74cfe01
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-07-24 01:30:24 -0400

    Update palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+def isPalindrome(num):
+    # if num<0:
+    #     return False
+    # string=str(num)
+    # length=len(string)
+    # if length%2 == 0 and num>0:
+
+    #     return length
+ string=str(num)
+ length=len(string)
+ for element in string(range(0,length/2)):
+     print(element)   
+res = isPalindrome(23)
+print(res)
+        #    python palindrome.py
+        #    python palindrome.py
