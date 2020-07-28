83b46d22d895004a36ecb178207996de80d21c67
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-07-28 00:25:28 -0400

    Update patternMatch.py

--- patternMatch.py
+++ patternMatch.py
@@ -5,3 +5,35 @@ class Solution:
         :type pattern: str
         :rtype: list of str
         '''
+        matches = []
+
+        for word in words:
+            if self.is_aligned(word, pattern):
+                matches.append(word)
+
+        return matches
+
+    def is_aligned(self, word, pattern):
+        if len(word) != len(pattern):
+            return False
+
+        word_to_pattern = [0] * 256
+        pattern_to_word = [0] * 256
+
+        for i in range(0, len(word)):
+            word_char = ord(word[i])
+            pattern_char = ord(pattern[i])
+
+            if word_to_pattern[word_char] == 0:
+                word_to_pattern[word_char] = pattern_char
+
+            if pattern_to_word[pattern_char] == 0:
+                pattern_to_word[pattern_char] = word_char
+
+            if (
+                word_to_pattern[word_char] != pattern_char or
+                pattern_to_word[pattern_char] != word_char
+            ):
+                return False
+
+        return True
